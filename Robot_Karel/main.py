6c2b620c10087fbc108ae3d85e265dfc0bf70acf
Ben <ben.madou@atheneummerlbeke.be>
2026-06-15 12:26:00 +0200

    Working model

--- Robot_Karel/main.py
+++ Robot_Karel/main.py
@@ -23,14 +23,6 @@ def get_direction():
   keymap = {"z": "up", "s": "down", "q": "left", "d": "right"}
   return keymap.get(typed)
 
-
-finish = random_cell()
-
-karel = random_cell()
-while karel == finish:
-  karel = random_cell()
-
-
 def move(karel,direction):
   moves = {"up": (0, -1), "down": (0, 1), "left": (-1, 0), "right": (1, 0)}
   x, y = karel
@@ -44,8 +36,28 @@ def move(karel,direction):
     karel = (new_x, new_y)
   return karel
 
+def draw(karel, finish):
+  for y in range(5):
+    row = ""
+    for x in range(5):
+      if (x, y) == karel:
+        row += "K"
+      elif (x, y) == finish:
+        row += "F"
+      else:
+        row += "."
+    print(row)
+  print()
 
-print(move((2, 2), "up"))
-print(move((0, 0), "up"))
-print(move((2, 2), None))
+finish = random_cell()
 
+karel = random_cell()
+while karel == finish:
+  karel = random_cell()
+
+draw(karel, finish)
+while karel != finish:
+  karel = move(karel, get_direction())
+  draw(karel, finish)
+
+print("Karel made it!")
